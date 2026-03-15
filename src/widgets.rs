8a9650c57a9f280d8ee6afa84e3941da05989229
Garry O'Donnell <garry.euan.odonnell@ntlworld.com>
2026-03-15 10:50:47 +0000

    feat: add help bar widget which renders keybinds in a single row

--- src/widgets.rs
+++ src/widgets.rs
@@ -1,12 +1,14 @@
 use crate::KeyBind;
+use itertools::Itertools;
 use ratatui_core::{
     buffer::Buffer,
     layout::{Constraint, Rect},
+    text::Line,
     widgets::Widget,
 };
 use ratatui_widgets::table::{Cell, Row, Table};
 
-/// A widget displaying a table of bound keys and their description
+/// A [`Widget`] displaying a table of bound keys and their description
 pub struct Help<'k> {
     keybinds: &'k [KeyBind],
 }
@@ -32,5 +34,34 @@ impl<'k> Widget for Help<'k> {
             ])
         });
         Table::new(rows, [Constraint::Min(8), Constraint::Fill(1)]).render(area, buf);
+    }
+}
+
+/// A [`Widget`] displaying a single row of bound keys and their descriptions
+pub struct HelpBar<'k> {
+    keybinds: &'k [KeyBind],
+}
+
+impl<'k> HelpBar<'k> {
+    /// Construct a [`HelpBar`] [`Widget`] from a collection of [`KeyBind`]s, typically obtained by
+    /// inspecting the metadata as [`crate::KeyMap::KEYBINDS`] or
+    /// [`crate::DynKeyMap::keybinds`]
+    pub fn new(keybinds: &'k [KeyBind]) -> Self {
+        Self { keybinds }
+    }
+}
+
+impl<'k> Widget for HelpBar<'k> {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        #![allow(unstable_name_collisions)]
+        Line::from_iter(
+            self.keybinds
+                .iter()
+                .map(|KeyBind { key, description }| {
+                    format!("{}: {}", description.unwrap_or_default(), key)
+                })
+                .intersperse(" | ".to_string()),
+        )
+        .render(area, buf);
     }
 }
