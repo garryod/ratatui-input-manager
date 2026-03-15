fb2092b68ac5f40f63b9a0dec4cb8650e31c498d
Garry O'Donnell <garry.euan.odonnell@ntlworld.com>
2026-03-15 10:34:13 +0000

    feat: add metadata about which keys are abound

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,40 @@
 #![warn(missing_docs)]
 #![doc = include_str!("../README.md")]
 
-use crossterm::event::Event;
+use crossterm::event::{Event, KeyCode};
 pub use ratatui_input_manager_derive::keymap;
+
+/// Key binding metadata, including the handlded key press and a description of behaviour
+pub struct KeyBind {
+    /// The key which is handled by this binding
+    pub key: KeyCode,
+    /// A brief description of the expected behaviour
+    pub description: Option<&'static str>,
+}
 
 /// A mapping between keybinds and methods on the type which mutate the state
 pub trait KeyMap {
+    /// Metadata about the key presses which are handled
+    const KEYBINDS: &'static [KeyBind];
+
     /// Handle a [`crossterm::event::Event`] by calling the appropriate handler method
     fn handle(&mut self, event: &Event);
+}
+
+/// A dyn compatible equivilent to [`KeyMap`]
+#[expect(missing_docs)]
+pub trait DynKeyMap {
+    fn keybinds(&self) -> &'static [KeyBind];
+
+    fn handle(&mut self, event: &Event);
+}
+
+impl<T: KeyMap> DynKeyMap for T {
+    fn keybinds(&self) -> &'static [KeyBind] {
+        T::KEYBINDS
+    }
+
+    fn handle(&mut self, event: &Event) {
+        self.handle(event)
+    }
 }
